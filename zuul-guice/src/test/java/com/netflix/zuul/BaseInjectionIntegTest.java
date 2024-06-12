402b0276fc27e8a92b56cb67bf4215291699f501
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-06-12 13:27:05 +0530

    Update BaseInjectionIntegTest.java

--- zuul-guice/src/test/java/com/netflix/zuul/BaseInjectionIntegTest.java
+++ zuul-guice/src/test/java/com/netflix/zuul/BaseInjectionIntegTest.java
@@ -14,7 +14,7 @@
  *      limitations under the License.
  */
 package com.netflix.zuul;
-// jrfk
+// jrfkf
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.netflix.zuul.init.InitTestModule;
