28193f5ecde6f6d04e3c364fdb6cd30ee5b7af08
ShobhitSingh11 <shobhit.singh@harness.io>
2024-07-08 14:55:56 +0530

    Added uts

--- zuul-processor/src/main/java/com/netflix/zuul/filters/processor/Calculator.java
+++ zuul-processor/src/main/java/com/netflix/zuul/filters/processor/Calculator.java
@@ -1,2 +1,7 @@
-package com.netflix.zuul.filters.processor;public class Calculator {
+package com.netflix.zuul.filters.processor;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
--- zuul-processor/src/test/java/com/netflix/zuul/filters/processor/unit/CalculatorUnitTest.java
+++ zuul-processor/src/test/java/com/netflix/zuul/filters/processor/unit/CalculatorUnitTest.java
@@ -1,2 +1,29 @@
-package com.netflix.zuul.filters.processor.unit;public class CalculatorUnitTest {
+package com.netflix.zuul.filters.processor.unit;
+
+import com.netflix.zuul.filters.processor.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorUnitTest {
+
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(3, 4);
+        assertEquals(7, result, "Sum of 3 and 4 should be 7");
+    }
+
+    @Test
+    public void testSumWithNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(-3, -4);
+        assertEquals(-7, result, "Sum of -3 and -4 should be -7");
+    }
+
+    @Test
+    public void testSumWithZero() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(0, 5);
+        assertEquals(5, result, "Sum of 0 and 5 should be 5");
+    }
 }
