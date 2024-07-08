e74d4bd6a635d165485abffb35f589be51079d72
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-07-08 15:47:37 +0530

    Update CalculatorUnitTest.java

--- zuul-processor/src/test/java/com/netflix/zuul/filters/processor/unit/CalculatorUnitTest.java
+++ zuul-processor/src/test/java/com/netflix/zuul/filters/processor/unit/CalculatorUnitTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorUnitTest {
 
+    @Test
     public void testSum() {
         Calculator calculator = new Calculator();
         int result = calculator.sum(3, 4);
